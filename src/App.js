b3de8b7a22996868de782a0faaf9d27e6244145d
Park Sang Jun <iteach12@gmail.com>
2020-11-12 06:41:39 +0900

    Map준비작업

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
 
+function Food(props){
+  console.log(props);
+  return (<div>
+  <h1>I love {props.fav}.</h1>
+  </div>
+  
+  );
+}
+
+
+const foodILie = [
+  {
+    name: 'kimchi',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.6Mea2CXE0T5R9pbPWubGUgHaEK%26pid%3DApi&f=1'
+  },
+  {
+    name: 'sam',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.GLxnJpKDohpsrPZQGrGywQHaE7%26pid%3DApi&f=1'
+  },
+  {
+    name: 'kimbab',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.7-rpW2ynK3Mq89GWd2Q36QHaE8%26pid%3DApi&f=1'
+  }
+];
 
 
 function App() {
-  return <div><h1>
-    Hello!!
-  </h1>
-  </div>;
+  return (<div><h5>
+    Hello
+  </h5>
+  
+
+  </div>);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 
+
+//리액트는 최종적으로 하나의 컴포넌트를 그려야 한다.
+//여러 개의 컴포넌트는 하나로 합친 다음 그려야 할 듯.
+//예를들어 포테이토 컴포넌트는 앱 컴포넌트에 넣어서 앱 컴포넌트를 
+//그리면 됨.
 ReactDOM.render(<App />, document.getElementById('root'));
 
 
