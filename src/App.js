5ba31c574a7ce617ead7811f3267f168b20b0e0f
Park Sang Jun <iteach12@gmail.com>
2020-11-13 06:55:15 +0900

    props_study_109

--- src/App.js
+++ src/App.js
@@ -1,39 +1,59 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Food(props){
-  console.log(props);
+
+function Food({name, picture, rating}){
+  
+  
   return (<div>
-  <h1>I love {props.fav}.</h1>
+  <h2>I love {name}.</h2>
+  <h4>{rating}/5.0</h4>
+  <img src={picture} alt={name} />
+
   </div>
   
   );
 }
 
 
-const foodILie = [
+const foodILike = [
   {
+    id:1,
     name: 'kimchi',
-    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.6Mea2CXE0T5R9pbPWubGUgHaEK%26pid%3DApi&f=1'
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.6Mea2CXE0T5R9pbPWubGUgHaEK%26pid%3DApi&f=1',
+    rating: 5
   },
   {
+    id:2,
     name: 'sam',
-    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.GLxnJpKDohpsrPZQGrGywQHaE7%26pid%3DApi&f=1'
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.GLxnJpKDohpsrPZQGrGywQHaE7%26pid%3DApi&f=1',
+    rating: 2
   },
   {
+    id:3,
     name: 'kimbab',
-    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.7-rpW2ynK3Mq89GWd2Q36QHaE8%26pid%3DApi&f=1'
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.7-rpW2ynK3Mq89GWd2Q36QHaE8%26pid%3DApi&f=1',
+    rating: 1
   }
 ];
 
 
 function App() {
-  return (<div><h5>
-    Hello
-  </h5>
   
-
-  </div>);
+  return(
+    <div>
+      {foodILike.map(dish => <Food  key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />)}
+    </div>
+    );
 }
+
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
+
 
 export default App;
 
