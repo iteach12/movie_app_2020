0404c3305cf22d6b05d84a03820160201ff3ed98
Park Sang Jun <iteach12@gmail.com>
2020-11-15 15:02:42 +0900

    #07_css들어감

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { render } from '@testing-library/react';
 import React from 'react';
 import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
+
 
 class App extends React.Component{
 
@@ -33,12 +36,28 @@ class App extends React.Component{
 
     const {isLoading, movies} = this.state;
 
-  return(<div>
-    {isLoading ? 'Loading...' : movies.map((movie)=>{
-      console.log(movie);
-      return;
-    })}
-    </div>);
+  return(<section class="container">
+    {isLoading ? (
+      <div class="loader">
+      <span class="loader_textr">'Loading...'</span>
+      </div>
+      ) : (
+      <div class="movies">
+      {movies.map(movie=>(
+    
+      <Movie
+        key={movie.id}
+        id={movie.id}
+        year={movie.year}
+        title={movie.title}
+        summary={movie.summary}
+        poster={movie.medium_cover_image}
+       />
+
+      ))}
+    </div>
+    )}
+    </section>);
   }
 }
 
