4154367976f5351e3da89b3e9dfce5d7d38ccdff
Park Sang Jun <iteach12@gmail.com>
2020-11-19 06:32:14 +0900

    최종 완성 코드

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css'
+import {Link} from 'react-router-dom';
 
 function Movie({title, year, summary, poster, genres}){
 return(
     <div className="movie">
+        
+        <Link to={{
+            pathname: '/movie-detail',
+            state: {year, title, summary, poster, genres},
+            }}>
+            
+        
+        
+        
         <img src={poster} alt={title} title={title} />
         <div className="movie__data">
             <h3 className="movie__title">{title}</h3>
@@ -16,9 +26,8 @@ return(
 
             </ul>
             <p className="movie__summary"> {summary.slice(0, 100)}...</p>
-
-
     </div>
+    </Link>
     </div>
 
 );
