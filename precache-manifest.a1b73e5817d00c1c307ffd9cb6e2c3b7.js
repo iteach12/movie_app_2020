dee4ab8abf5558a32f1b73a3467aa21bea9286d0
Park Sang Jun <iteach12@gmail.com>
2020-11-19 06:35:18 +0900

    Updates

---
+++ precache-manifest.a1b73e5817d00c1c307ffd9cb6e2c3b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3da86b513f65bdae685c4bdcdd8c0dae",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "d5380cfa9d058f8206c4",
+    "url": "/movie_app_2020/static/css/main.1d93baa7.chunk.css"
+  },
+  {
+    "revision": "2f24412d0755e60857ba",
+    "url": "/movie_app_2020/static/js/2.d5e88aa0.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/movie_app_2020/static/js/2.d5e88aa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5380cfa9d058f8206c4",
+    "url": "/movie_app_2020/static/js/main.d58c0017.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
